033edbeb830a175cfe7d24cbc7e15491d046b489
Georgios Tertlidis <tertlidis@protonmail.com>
2025-07-10 23:26:13 +0200

    new

---
+++ src/sections/Projects/Projects.jsx
@@ -0,0 +1,42 @@
+import styles from './ProjectsStyles.module.css';
+import viberr from '../../assets/viberr.png';
+import freshBurger from '../../assets/fresh-burger.png';
+import hipsster from '../../assets/hipsster.png'
+import fitLift from '../../assets/fitlift.png'
+import ProjectCard from '../../common/ProjectCard';
+
+function Projects() {
+  return (
+    <section id="projects" className={styles.container}>
+      <h1 className="sectionTitle">Projects</h1>
+      <div className={styles.projectsContainer}>
+        <ProjectCard  
+          src={viberr} 
+          link="https://github.com/Ade-mir/company-landing-page-2"
+          h3="Project 1"
+          p="Project 1"
+        />
+        <ProjectCard  
+          src={freshBurger} 
+          link="https://github.com/Ade-mir/company-landing-page-2"
+          h3="Project 2"
+          p="Project 2"
+        />
+        <ProjectCard  
+          src={hipsster} 
+          link="https://github.com/Ade-mir/company-landing-page-2"
+          h3="Project 3"
+          p="Project 3"
+        />
+        <ProjectCard  
+          src={fitLift} 
+          link="https://github.com/Ade-mir/company-landing-page-2"
+          h3="Project 4"
+          p="Project 4"
+        />
+      </div>
+    </section>
+    );
+  }
+
+export default Projects;
